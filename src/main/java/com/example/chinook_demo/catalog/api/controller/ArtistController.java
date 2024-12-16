21240e6ed36a16454d78557baa00d9292ee4e8ad
Burcu Bayık <burcu.bayik98@gmail.com>
2024-12-16 09:05:50 +0300

    refactor: change returnType of findArtistByArtistId method in ArtistController

--- src/main/java/com/example/chinook_demo/catalog/api/controller/ArtistController.java
+++ src/main/java/com/example/chinook_demo/catalog/api/controller/ArtistController.java
@@ -3,6 +3,7 @@ package com.example.chinook_demo.catalog.api.controller;
 import com.example.chinook_demo.catalog.application.dto.ArtistDto;
 import com.example.chinook_demo.catalog.application.service.ArtistService;
 import com.example.chinook_demo.catalog.domain.model.ArtistId;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class ArtistController {
     }
 
     @GetMapping()
-    public ArtistDto findArtistByArtistId(@RequestBody ArtistId artistId){
-        return artistService.findArtistByArtistId(artistId);
+    public ResponseEntity<ArtistDto> findArtistByArtistId(@RequestBody ArtistId artistId){
+        return ResponseEntity.ok(artistService.findArtistByArtistId(artistId));
     }
 }
